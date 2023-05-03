f03a33261f0dd7b2f7919c12a604220c3f0637aa
hirakuuuu <m.hiraku1129@gmail.com>
2023-05-03 23:27:54 +0900

    [add] めちゃくちゃ雑だけど、フィールド増設機能実装

---
+++ Assets/Scripts/GameScene/FieldScript.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldScript : MonoBehaviour
+{
+    // フィールドのサイズ
+    private float fieldSize;
+    // フィールドの辺の長さ
+    private float fieldLength;
+    // フィールドの最大の大きさ
+    private float fieldMaxSize;
+
+    // フィールドのゲームオブジェクト
+    public GameObject fieldTile;
+
+    void Start() 
+    {
+        // フィールドのサイズを設定
+        fieldSize = 1;
+        // フィールドの辺の長さを設定
+        fieldLength = fieldSize * 10;
+        // フィールドの最大の大きさを設定
+        fieldMaxSize = 10;
+        // フィールドを取得
+        fieldTile = GameObject.FindWithTag("FieldTile");
+    }
+
+    public void ExtensionFieldSize()
+    {
+        // フィールドが最大の大きさだったらリターン
+        if (fieldSize == fieldMaxSize)
+        {
+            return;
+        }
+        // フィールドのサイズを拡張
+        fieldSize++;
+        // フィールドの辺の長さを拡張
+        fieldLength = fieldSize * 10;
+        // フィールドの大きさを変更
+        fieldTile.transform.localScale = new Vector3(fieldSize, 1, fieldSize);
+    }
+}
