2dd066bb9215f70057b8b1d9c65755830af45ea8
Makochaaan <arowana326.315@gmail.com>
2023-05-05 14:25:12 +0900

    [fix]明度調整及びタワー発光

--- Assets/Scripts/GameScene/FlashTower.cs
+++ Assets/Scripts/GameScene/FlashTower.cs
@@ -8,7 +8,8 @@ public class FlashTower : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {    
-
+        GetComponent<MeshRenderer>().materials[1].EnableKeyword("_EMISSION");
+        GetComponent<MeshRenderer>().materials[1].SetColor("_EmissionColor", Color.yellow);
         
     }
 
